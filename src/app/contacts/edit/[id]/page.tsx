1117b07527aed2d161bf01b95ab8b2bf74477122
TracerStudySmekdapapa <88016676+Thebibii@users.noreply.github.com>
2024-12-13 12:34:34 +0700

    Update page.tsx

--- src/app/contacts/edit/[id]/page.tsx
+++ src/app/contacts/edit/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getContactById } from "@/lib/data";
 import { notFound } from "next/navigation";
 
 const UpdateContactPage = async ({ params }: { params: { id: string } }) => {
-  const id = await params.id;
+  const { id } = params;
   const contact = await getContactById(id);
 
   if (!contact) {
